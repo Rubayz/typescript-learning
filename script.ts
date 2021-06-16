7d54877e5dbdc3c3324b09c712563abeaedc59c7
Rubayz <rubayzaf2007@gmail.com>
2021-06-16 19:54:10 +0600

    consoled and working with interfaces

---
+++ script.ts
@@ -0,0 +1,15 @@
+// Learned logging in the console
+const greet = "Hello!";
+
+console.log(greet);
+
+//Learned stuffs with interfaces
+interface User {
+    name: string,
+    id: number
+};
+
+const user: User = {
+    name: "mario",
+    id: 5
+}
